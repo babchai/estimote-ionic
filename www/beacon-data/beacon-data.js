d9b60ed9bf9ed283a5cf8422ac909f000f20c68d
sai chang choon <babchai@hotmail>
2015-03-20 00:10:44 +0800

    first commit .

---
+++ www/beacon-data/beacon-data.js
@@ -0,0 +1,26 @@
+window.ApplicationBeaconData = {
+	"18774_17060": {
+		"title": {
+			"line1":"[Up to 50% Off]",
+			"line2":"iPhone 4S from RM699 for 32GB"
+		},
+		"description": "<li>32GB iPhone 4S for RM699 instead of RM950</li> <li>64GB iPhone 4S for RM799 instead of RM1,599</li>",
+		"image": "https://s3-ap-southeast-1.amazonaws.com/hoboworkshop/beacon/images/18774_17060.jpg"
+	},
+	"20718_65430": {
+		"title": {
+			"line1" : "[Up to 51% Off]",
+			"line2" : "Tempered Glass Digital Weighing Scale from RM39"
+		},
+		"description": "<li>1 unit for RM39 instead of RM69.90</li> <li>2 units for RM69  instead of RM139.80 (only RM34.50 per unit)</li>",
+		"image": "https://s3-ap-southeast-1.amazonaws.com/hoboworkshop/beacon/images/20718_65430.jpg"
+	},
+	"25314_34247": {
+		"title": {
+			"line1" : "[52% Off]",
+			"line2" : "GAMMAX 21-Speed Foldable Mountain Bike for RM479"
+		},
+		"description": "GAMMAX 21-speed mountain bike for RM479 instead of RM999.90",
+		"image": "https://s3-ap-southeast-1.amazonaws.com/hoboworkshop/beacon/images/25314_34247.jpg"
+	}
+};
